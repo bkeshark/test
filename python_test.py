f83871dd96c2dcbef6c87624bb14b4c7f9f25ae1
bkeshark <bhagirath.kesharkhane@gmail.com>
2020-05-01 23:44:11 +1000

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,1 +1,8 @@
 print('this is testing python code')
+
+def string = 'this is string'
+
+string.
+
+
+docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 docker.bintray.io/jfrog/artifactory-oss:latest
